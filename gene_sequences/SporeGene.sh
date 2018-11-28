a96d5c74dfaddb6e8119bcf0a273c40f0eaca207
Alicia Susi <asusi@nd.edu>
2018-11-27 20:23:37 -0500

    Shell script for Problem 1

--- gene_sequences/SporeGene.sh
+++ gene_sequences/SporeGene.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 for file in sporecoat0*.fasta
-	do
+        do
 		sed -e '$s/$/\n/' $file
 	done > sporecoatAll.fasta
+for file in transporter0*.fasta
+	do
+		sed -e '$s/$/\n/' $file						       
+	done > transporterAll.fasta
+
+for file in *All.fasta
+	do
+	        /afs/nd.edu/user17/asusi/local/bin/muscle -in $file -out /afs/nd.edu/user17/asusi/local/bin/IBC_EX11/$file.align
+	done
+
