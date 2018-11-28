78080b8f320741b58aa477a3e9a4f40ea4a0c706
Patricia Portmann <pportman@nd.edu>
2018-11-27 21:28:20 -0500

    What I did last Monday

---
+++ seqAlign.sh
@@ -0,0 +1,25 @@
+# Exercise 10 : Patricia Portmann
+# Aligns the sequences of the "sporecoat" .fasta files
+# Aligns the sequences of the "transporter" .fasta files 
+# usage: bash seqAlign.sh *.fasta
+
+# appends all the genes from the spore file into one .fasta file
+for file in spore*
+do
+	cat $file >> all_spore.fasta
+	echo >> all_spore.fasta
+done
+
+# uses muscle to find sequence alignment in the combined spore file
+../muscle3.8.31_i86linux64 -in all_spore.fasta -out all_spore.fasta.align
+
+
+# appends all the genes from the transporter files into one .fasta file
+for file in transporter*
+do
+	cat $file >> all_transpo.fasta
+	echo >> all_transpo.fasta
+done
+
+# uses muscle to find sequence alignment in the combined transporter file
+../muscle3.8.31_i86linux64 -in all_transpo.fasta -out all_transpo.fasta.align
